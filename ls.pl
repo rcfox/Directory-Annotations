9ef09d30eb7dc3949ead64c6a7d46686430e68e3
Ryan Fox <ryan@caffeine.(none)>
2009-11-08 03:18:03 -0500

    Initial commit.

---
+++ ls.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Cwd;
+
+my $pwd = cwd();
+
+# Check to see if any of the arguments exist as directories.
+my @directories = grep { -d $_ || -d "$pwd/$_" } @ARGV;
+
+# Use anything beginning with a - as an argument for ls
+my @arguments = grep { /^-/ } @ARGV;
+
+my $ls_arguments = join(' ', @arguments);
+
+if (!@directories)
+{
+    # If no directories were specified, and all of the other arguments are valid, work on the current directory.
+    if (@arguments == @ARGV)
+    {
+        push @directories, ".";
+    }
+    else
+    {
+        # Enumerate the invalid directories
+        foreach(grep { !($_ ~~ @directories || $_ ~~ @arguments) } @ARGV)
+        {
+            print "ls: cannot access $_: No such file or directory\n";
+        }
+    }
+}
+
+foreach(@directories)
+{
+    # Correct any spaces in filenames.
+    s/ /\\ /;
+
+    # Call ls on each directory separately so that we can output the .ls file beside it.
+    if (system("ls $_ $ls_arguments") == 0)
+    {
+        # Open the .ls file, if it exists. Otherwise, go on to the next directory.
+        open(my $file, '<', "$_/.ls") or next;
+        print "---\n";
+        print <$file>;
+        print "---\n";
+        close($file);
+    }
+}
